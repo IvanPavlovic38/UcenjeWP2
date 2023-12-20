ee7c0d320687e316d918845d6d7435b3c04e4490
IvanPavlovic38 <ipavlovic6437@gmail.com>
2023-12-21 00:41:07 +0100

    Update zadatak1.sql

--- zadatak1.sql
+++ zadatak1.sql
@@ -6,28 +6,58 @@ use master;
 
 create table zupani(
 sifra int not null primary key identity(1,1),
-ime varchar(50),
-prezime varchar(50),
+ime varchar(50) not null,
+prezime varchar(50) not null,
 );
 
 create table zupanije(
 sifra int not null primary key identity(1,1),
-naziv varchar(50),
+naziv varchar(50) not null,
 zupan int
 );
 
 create table opcine(
 sifra int not null primary key identity(1,1),
 zupanija int,
-naziv varchar(50)
+naziv varchar(50) not null
 );
 
 create table mjesta(
 sifra int not null primary key identity(1,1),
 opcina int,
-naziv varchar(50)
+naziv varchar(50) not null
 );
 
-alter table mjesta add foreign key (opcina) references opcina(sifra);
+alter table mjesta add foreign key (opcina) references opcine(sifra);
 alter table opcine add foreign key (zupanija) references zupanije(sifra);
 alter table zupanije add foreign key (zupan) references zupani(sifra);
+
+insert into zupani(ime, prezime) values
+('Stjepan','Kozic'),
+('Zeljko','Kolar'),
+('Ivan','Celjak'),
+('Darko','Koren');
+
+insert into zupanije(naziv) values
+('Zagrebacka zupanija'),
+('Krapinsko-zagorska zupanija'),
+('Sisacko-moslavacka zupanija'),
+('Koprivnicko-krizevacka zupanija');
+
+insert into opcine(naziv) values
+('Bale'),
+('Cerna'),
+('Dicmo'),
+('Gradac');
+
+insert into mjesta(naziv) values
+('')
+('')
+('')
+('');
+
+select * from zupani;
+
+select * from zupanije;
+
+select * from opcine;
