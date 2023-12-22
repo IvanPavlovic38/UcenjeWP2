0459144ca92e029ae0fba7b519531fc87e7a856c
IvanPavlovic38 <ipavlovic6437@gmail.com>
2023-12-22 20:42:42 +0100

    programi sa predavanja

---
+++ EdunovaWP2/CSHARP/Ucenje/UcenjeCS/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using UcenjeCS;
+
+//E01UlazIzlaz.Izvedi();
+
+//E02VarijableTipoviPodatakaOperatori.Izvedi();
+
+//E03UvjetnaGrananja.Izvedi();
+
+E04Nizovi.Izvedi();
+
+//E05ForPetlja.Izvedi();
