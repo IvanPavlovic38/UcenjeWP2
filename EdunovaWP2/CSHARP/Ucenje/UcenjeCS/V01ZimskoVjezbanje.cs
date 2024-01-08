0fbe99deb99a73fcad540169c727f010f12ed668
IvanPavlovic38 <ipavlovic6437@gmail.com>
2024-01-08 21:31:08 +0100

    Update V01ZimskoVjezbanje.cs
    zadatak 4

--- EdunovaWP2/CSHARP/Ucenje/UcenjeCS/V01ZimskoVjezbanje.cs
+++ EdunovaWP2/CSHARP/Ucenje/UcenjeCS/V01ZimskoVjezbanje.cs
@@ -9,15 +9,35 @@
                 Console.WriteLine(i+1);
             }
 
-            for (int i = 99; i>0; i--)
+            Console.WriteLine(" ");
+
+            for (int i = 100; i>0; i--)
             {
-                Console.WriteLine(i+1);
+                Console.WriteLine(i);
             }
+
+            Console.WriteLine(" ");
 
             for (int i = 7; i<100; i = i+7)
             {
                 Console.WriteLine(i);
             }
+
+            Console.WriteLine(" ");
+
+            int broj;
+            int br=0;
+
+            for ( ; ; )
+            {
+                broj = int.Parse( Console.ReadLine() );
+                br++;
+                if (broj > 100)
+                {
+                    break;
+                }                
+            }
+            Console.WriteLine(br);
         }
     }
 }
