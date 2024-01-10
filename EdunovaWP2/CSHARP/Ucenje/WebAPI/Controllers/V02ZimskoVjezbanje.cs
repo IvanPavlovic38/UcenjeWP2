7a55b0ce0397744431cef4e05fd2f620d53798c2
IvanPavlovic38 <ipavlovic6437@gmail.com>
2024-01-10 23:53:38 +0100

    V02ZimskoVjezbanje
    prvi zadatak

---
+++ EdunovaWP2/CSHARP/Ucenje/WebAPI/Controllers/V02ZimskoVjezbanje.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("V02")]
+    public class V02ZimskoVjezbanje:ControllerBase
+    {
+        [HttpGet]
+        [Route("zad1")]
+        public int Zad1( int brojevi)
+        {
+            int zbroj=0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                zbroj = zbroj + i + 1;
+            }
+
+            return zbroj;
+            
+        }
+
+        
+
+
+       
+    }
+}
