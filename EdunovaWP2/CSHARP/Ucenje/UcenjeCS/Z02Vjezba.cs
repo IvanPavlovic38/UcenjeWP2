69e0647a915d47dc8ab5913fea37030bfe6053ce
IvanPavlovic38 <ipavlovic6437@gmail.com>
2024-01-26 23:51:44 +0100

    Z02Vjezba

--- EdunovaWP2/CSHARP/Ucenje/UcenjeCS/Z02Vjezba.cs
+++ EdunovaWP2/CSHARP/Ucenje/UcenjeCS/Z02Vjezba.cs
@@ -9,76 +9,120 @@ namespace UcenjeCS
 {
     internal class Z02Vjezba
     {
-        public static int Zbroj(int broj, int suma)
-        {
-            suma += broj;
-
-            return suma;
-        }
-
-        public static int Najmanji(int broj, int min)
-        {
-            if (broj < min)
-            {
-                min = broj;
-            }
-
-            return min;
-        }
-
-        public static int Najveci(int broj, int max)
-        {
-            if (broj > max)
-            {
-                max = broj;
-            }
-
-            return max;
-        }
-
-        public static double Prosjek(int suma, int br)
-        {
-            return (double)suma / br;
-        }
-
+        
         public static void Izvedi()
         {
-            int suma = 0;
-            int min = 0;
-            int max = 0;
-            int br = 0;
-            while (true)
+            Console.WriteLine("Unesite brojeve (unos zavrsava s -1):");
+            try
             {
-                Console.Write("Unesite broj (-1 za kraj): ");
-                try
+                double[] brojevi = UnesiBrojeve();
+                if (brojevi.Length > 0)
                 {
-                    int broj = int.Parse(Console.ReadLine());
+                    double zbroj = Zbroj(brojevi);
+                    double najmanji = Najmanji(brojevi);
+                    double najveci = Najveci(brojevi);
+                    double prosjek = Prosijek(brojevi);
 
-                    if (broj == -1)
-                    {
-                        break;
-                    }
-
-                    Zbroj(broj, suma);
-                    Najmanji(broj, min);
-                    Najveci(broj, max);
-                    br++;
+                    IspisiRezultate(zbroj, najmanji, najveci, prosjek);
                 }
-                catch (FormatException)
+                else
                 {
-                    Console.WriteLine("Neispravan unos, molimo unesite cijeli broj.");
+                    Console.WriteLine("Niste unijeli nijedan broj.");
                 }
             }
-
-            Console.WriteLine("Zbroj unesenih brojeva je: " + suma);
-            Console.WriteLine("Najmanji uneseni broj je: " + min);
-            Console.WriteLine("Najveći uneseni broj je: " + max);
-            Console.WriteLine("Prosjek unesenih brojeva je: " + Prosjek(suma, br));
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Greška: {ex.Message}");
+            }
         }
 
+        static double[] UnesiBrojeve()
+        {
+            var brojevi = new System.Collections.Generic.List<double>();
+            while (true)
+            {
+                string unos = Console.ReadLine();
+                if (unos == "-1")
+                {
+                    break;
+                }
 
+                if (!double.TryParse(unos, out double broj))
+                {
+                    throw new Exception("Neispravan unos. Molimo unesite broj.");
+                }
 
+                brojevi.Add(broj);
+            }
 
+            return brojevi.ToArray();
+        }
+
+        static double Zbroj(double[] brojevi)
+        {
+            double zbroj = 0;
+            foreach (var broj in brojevi)
+            {
+                zbroj += broj;
+            }
+            return zbroj;
+        }
+
+        static double Najmanji(double[] brojevi)
+        {
+            if (brojevi.Length == 0)
+            {
+                throw new Exception("Nema brojeva za pronalazak najmanjeg broja.");
+            }
+
+            double najmanji = brojevi[0];
+            foreach (var broj in brojevi)
+            {
+                if (broj < najmanji)
+                {
+                    najmanji = broj;
+                }
+            }
+            return najmanji;
+        }
+
+        static double Najveci(double[] brojevi)
+        {
+            if (brojevi.Length == 0)
+            {
+                throw new Exception("Nema brojeva za pronalazak najvećeg broja.");
+            }
+
+            double najveci = brojevi[0];
+            foreach (var broj in brojevi)
+            {
+                if (broj > najveci)
+                {
+                    najveci = broj;
+                }
+            }
+            return najveci;
+        }
+
+        static double Prosijek(double[] brojevi)
+        {
+            if (brojevi.Length == 0)
+            {
+                throw new Exception("Nema brojeva za izračun prosjeka.");
+            }
+
+            double zbroj = Zbroj(brojevi);
+            return zbroj / brojevi.Length;
+        }
+
+        static void IspisiRezultate(double zbroj, double najmanji, double najveci, double prosjek)
+        {
+            Console.WriteLine($"Zbroj: {zbroj}");
+            Console.WriteLine($"Najmanji broj: {najmanji}");
+            Console.WriteLine($"Najveći broj: {najveci}");
+            Console.WriteLine($"Prosjek: {prosjek}");
+        }
+    
 
 
 
