4069e3a013c98e785afdc238f735be210b11fb2d
IvanPavlovic38 <ipavlovic6437@gmail.com>
2024-01-30 23:55:56 +0100

    Ljubavni Kalkulator

--- EdunovaWP2/CSHARP/Ucenje/UcenjeCS/LjubavniKalkulator.cs
+++ EdunovaWP2/CSHARP/Ucenje/UcenjeCS/LjubavniKalkulator.cs
@@ -18,5 +18,44 @@ namespace UcenjeCS
 
 
         }
+
+        public static int PretvoriUBrojeve(string TvojeIme, string SimpatijaIme)
+        {
+            string[] Unos = new string[TvojeIme.Length + SimpatijaIme.Length];
+            for(int i = 0; i < TvojeIme.Length; i++)
+            {
+                Unos[i] = TvojeIme[i];
+            }
+            for (int i = 0; i < SimpatijaIme.Length; i++)
+            {
+                Unos[i] = SimpatijaIme[i];
+            }
+            int[] Slova = new int[Unos.Length];
+            int Index = 0;
+            int Ukupno;
+            foreach (char c in Unos)
+            {
+                Ukupno = 0;
+                foreach (char cc in Unos)
+                {
+                    Ukupno++;
+                }
+                Slova[Index++] = Ukupno;
+            }
+            return Slova;
+        }
+
+        public static int IzracunajKompatibilnost(int Slova)
+        {
+            int[] Rezultat = new int[]; 
+            for (int i; i<;i++)
+            {
+                
+            }
+        }
+
+
+
+
     }
 }
