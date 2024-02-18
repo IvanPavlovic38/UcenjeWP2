31d99c8a967b81181bd9842383e037c8a498962f
IvanPavlovic38 <ipavlovic6437@gmail.com>
2024-02-18 23:57:32 +0100

    Update streaming app console app

---
+++ EdunovaWP2/CSHARP/Ucenje/UcenjeCS/StreamingApp/Prijava.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS.StreamingApp
+{
+    internal class Prijava
+    {
+        private void PozdravnaPoruka()
+        {
+            Console.WriteLine("Dobrodosli u streaming app");
+            Console.WriteLine("Upisite korisnicko ime, lozinku i email");
+        }
+
+        private void UpisiKorisnickoIme()
+        {
+            KorisnickoIme = Pomocno.UcitajString("Korisničko ime: ");
+        }
+
+        private void UpisiLozinku()
+        {
+            Lozinka = Pomocno.UcitajString("Lozinka: ");
+        }
+
+        private void UpisiEmail()
+        {
+            Email = Pomocno.UcitajString("Email: ");
+        }
+    }
+}
