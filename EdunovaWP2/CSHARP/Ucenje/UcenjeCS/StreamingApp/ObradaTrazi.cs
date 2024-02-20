1887ec7104217f52eb69579723055def5645c3a9
IvanPavlovic38 <ipavlovic6437@gmail.com>
2024-02-20 22:56:42 +0100

    update streaming app console app

--- EdunovaWP2/CSHARP/Ucenje/UcenjeCS/StreamingApp/ObradaTrazi.cs
+++ EdunovaWP2/CSHARP/Ucenje/UcenjeCS/StreamingApp/ObradaTrazi.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using UcenjeCS.StreamingApp.Model;
@@ -22,7 +23,32 @@ namespace UcenjeCS.StreamingApp
 
         public void PrikaziIzbornik()
         {
-            Console.WriteLine("Trazilica");
+            Console.WriteLine("Tražilica");
+            Console.WriteLine("1. Traži po nazivu");
+            Console.WriteLine("2. Traži po žanru");
+            Console.WriteLine("3. Traži po datumu izdavanja");
+
+            switch ()
+            {
+                case 1:
+                    TraziNaziv();
+                    PrikaziIzbornik();
+                    break;
+                case 2:
+                    TraziZanr();
+                    PrikaziIzbornik();
+                    break;
+                case 3:
+                    TraziDatum();
+                    PrikaziIzbornik();
+                    break;
+            }
+
+            private void TraziNaziv() { }
+
+            private void TraziZanr() { }
+
+            private void TraziDatum() { }
         }
     }
 }
