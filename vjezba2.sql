5239ae05b7fec6c9c834eea42e20623e099a3d66
IvanPavlovic38 <ipavlovic6437@gmail.com>
2024-06-01 23:55:33 +0200

    Update vjezba2.sql

--- vjezba2.sql
+++ vjezba2.sql
@@ -37,7 +37,7 @@ create table prijatelj(
 sifra int not null primary key identity(1,1), 
 modelnaocala varchar(37),
 treciputa datetime not null,
-ekstroventno bit not null,
+ekstrovertno bit not null,
 prviputa datetime,
 svekar int not null
 );
@@ -59,7 +59,7 @@ haljina varchar(33) not null,
 drugiputa datetime not null,
 suknja varchar(38),
 narukvica int,
-introventno bit,
+introvertno bit,
 majica varchar(40) not null,
 decko int
 );
@@ -76,15 +76,16 @@ alter table brat add foreign key (neprijatelj) references neprijatelj(sifra);
 alter table prijatelj add foreign key (svekar) references svekar(sifra);
 alter table decko_zarucnica add foreign key (decko) references decko(sifra);
 alter table decko_zarucnica add foreign key (zarucnica) references zarucnica(sifra);
+alter table prijatelj add foreign key (svekar) references svekar(sifra);
 
 insert into neprijatelj (kuna)
 values (560),
-values (780)
+values (780),
 values (630);
 
 insert into cura (introvertno)
 values (1),
-values (0)
+values (0),
 values (1);
 
 insert into decko_zarucnica ()
